c7b09248d8ed45d34db7b185b89d22fec44c6997
Dmitry Vrublevsky <dmitry.vrublevsky@printify.com>
2023-11-16 14:07:22 +0200

    init

---
+++ src/main/java/me/vrublevsky/carolinatest/carolinatest/CarolinatestApplication.java
@@ -0,0 +1,13 @@
+package me.vrublevsky.carolinatest.carolinatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarolinatestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarolinatestApplication.class, args);
+	}
+
+}
